f2d0a9fbbc11dfa10d575d74a84087a136f5947b
Eliyas <eliyasmd455@gmail.com>
2023-02-18 13:35:51 +0530

    new file added in new branch

---
+++ newfile.java
@@ -0,0 +1,12 @@
+class launch
+{
+   void m1()
+    {
+      system.out.println("hello java addicted");   
+    }
+     void m2()
+    {
+      system.out.println("hello java addicted");   
+    }
+}
+
